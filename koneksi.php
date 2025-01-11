f0841dbf8ac9b0234a21e48bb2a9b3cd0476a241
Vini Dwi Putri <vinidwipurtri@gmail.com>
2025-01-11 12:29:11 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+// koneksi.php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'comic_db';
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
